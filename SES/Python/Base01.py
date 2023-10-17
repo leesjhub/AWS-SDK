fe2b2a969c7cce3fa555e580bc056c01483fee9c
lsj <leesj03298@naver.com>
2023-10-17 14:42:26 +0900

    Add Base01 to SES

--- SES/Python/Base01.py
+++ SES/Python/Base01.py
@@ -32,11 +32,11 @@ def send_email(client,sender, recipient, subject, body):
 
 if __name__ == "__main__":
     # AWS SES 클라이언트 생성
+    region      = "your_aws_region"
     sender      = "your_sender@example.com"
     recipient   = "recipient@example.com"
     subject     = "이메일 테스트"
     body        = "이메일이 성공적으로 전송되었습니다."
-    region      = "your_aws_region"
     client      = boto3.client("ses", region_name = region)
 
     send_email(client, sender, recipient, subject, body)
