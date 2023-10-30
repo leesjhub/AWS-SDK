8d35412fd89981eb980b722da7888176ee8320ed
lsj <leesj03298@naver.com>
2023-10-31 00:21:53 +0900

    Add

--- QuickSight/Python/main.py
+++ QuickSight/Python/main.py
@@ -1,3 +1,14 @@
+import boto3
+from botocore.config import Config
+from quicksight import QuickSight
 
-if __name__ == "__main__":
-    
+# 프록시 설정을 추가합니다.
+# proxy_config = Config(proxies={'https': 'http://52.78.123.68:3128'})
+
+# botocore 세션을 생성합니다.
+# session = boto3.Session( config = proxy_config )
+# Quicksight 클라이언트 생성
+# quicksight_session = boto3.client('quicksight', region_name='ap-northeast-2', config = proxy_config)  # 본인의 리전에 맞게 변경하세요
+# register_user   = QuickSight.register_user(quicksight_session, 'default', 'ACCOUNTID', 'test@exmaple.com', 'QUICKSIGHT', 'READER', 'test' )
+# list_users      = QuickSight.list_users(quicksight_session, 'default', 'ACCOUNTID')
+# generate_embed_url_for_registered_user = QuickSight.generate_embed_url_for_registered_user(quicksight_session, 'ACCOUNTID', 'Namespace', 'QuickSightUser', 60)
---
+++ SES/Python/README.md
@@ -0,0 +1,15 @@
+## Base01.py
+
+Base01 수정 필요한 변수
+```python
+sender      = "your_sender@example.com"
+recipient   = "recipient@example.com"
+region      = "your_aws_region"
+```
+
+Base01 결과
+```bash
+$ python Base01.py 
+이메일이 성공적으로 전송되었습니다.
+Message ID:  010c018b3c255053-256e7a21-95c1-40b3-80a6-48cb0fc6d304-000000
+```
